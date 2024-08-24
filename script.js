44e6c8cab9e449e78ec27beff5bf15b236c1c465
meadoo <grim@graduate.org>
2024-08-24 09:08:08 -0500

    yippie script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log("haiii");
+
+let goto = (file, line) => {
+  window.parent.postMessage(
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+  );
+};
+// Get the file opening button from its class name
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+});
